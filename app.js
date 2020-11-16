60a259d3488d9de5a876a416e4f06c3028a77b94
LG BeTeng <brian7.teng@gmail.com>
2020-11-16 03:15:00 -0600

    final

--- app.js
+++ app.js
@@ -53,7 +53,20 @@ const storage = new GridFsStorage({
   const upload = multer({ storage });
 
 app.get('/', (req, res)=> {
-    res.render('index');
+    gfs.files.find().toArray((err, files)=>{
+        if(!files || files.length === 0){
+           res.render('index', {files: false});
+        } else{
+            files.map(file=>{
+                if(file.contentType==='/image/jpeg'||file.contentType==='image/png'||file.contentType==='/image/jpg'){
+                    file.isImage = true;
+                } else{
+                    file.isImage = false;
+                }
+            });
+            res.render('index', {files: files})
+        }
+    })
 });
 
 
@@ -99,6 +112,15 @@ app.get('/image/:filename', (req, res)=> {
                 err: 'not an img'
             })
         }
+    })
+})
+
+app.delete('/files/:id', (req, res)=>{
+    gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore)=>{
+        if(err){
+            return res.status(404).json({err: err})
+        }
+        res.redirect('/')
     })
 })
 
